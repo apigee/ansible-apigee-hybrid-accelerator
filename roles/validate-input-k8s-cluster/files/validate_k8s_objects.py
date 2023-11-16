62926c134b69849ff93a4409b22a16c30021da50
Payal Jindal <payaljindal050@gmail.com>
2023-11-16 18:16:29 +0530

    feat: added service account secret roles validations

--- roles/validate-input-k8s-cluster/files/validate_k8s_objects.py
+++ roles/validate-input-k8s-cluster/files/validate_k8s_objects.py
@@ -18,7 +18,10 @@ import argparse
 import json
 import os
 import sys
+import base64
+import requests
 
+from oauth2client.client import GoogleCredentials
 from kubernetes import client, config
 
 KUBECONFIG_FILE = os.getenv('KUBECONFIG')
@@ -65,6 +68,31 @@ def _finditem(obj, key, result=[]):
             if item is not None:
                 return item
 
+def get_roles(project_id):
+    credentials = GoogleCredentials.get_application_default()
+    access_token = credentials.get_access_token().access_token
+
+    headers = {}
+    headers['Authorization'] = f'Bearer {access_token}'
+    uri = f"https://cloudresourcemanager.googleapis.com/v1/projects/{project_id}:getIamPolicy?alt=json"
+
+    response = requests.post(uri, headers=headers)
+    if response.status_code == 200:
+        return True, response
+    else:
+        return False, {"error" : json.loads(response.text).get('error',{}).get('message','')}
+
+def get_sa_role(secret_name, namespace, bindings):
+    secret = api_instance.read_namespaced_secret(secret_name, namespace)
+    decoded_data = base64.b64decode(secret.data['client_secret.json'])
+    client_secret_data = json.loads(decoded_data)
+    if not client_secret_data.get('client_email', False):
+        return False, {"error" : f"ERROR: Incorrect secret data configuration. 'client_email' not present in {secret_name}'s secret data"}
+    
+    service_account_email = client_secret_data.get('client_email','')
+    bindings = [binding for binding in bindings if service_account_email in [member.split(':')[-1] for member in binding['members'] if member.startswith('serviceAccount:')]]
+    existing_roles = [binding['role'] for binding in bindings]
+    return True, existing_roles
 
 def main():
     parser = argparse.ArgumentParser(description='Validates Apigee Objects')
@@ -112,6 +140,45 @@ def main():
             if each_sa not in apigee_secrets:
                 validations.append(f"Service Account Secret: {each_sa} Not found in {apigee_namespace} namespace")  # noqa
 
+        apigee_components = ["mart", "runtime","udca", "logger", "metrics", "watcher","cassandra"]
+        project_id = input_data.get('gcp','').get('projectID','')
+        success, roles_response = get_roles(project_id)
+        
+        if success:
+            roles_map_file_path = 'roles_map.json'
+            roles_map = {}
+            try:
+                with open(roles_map_file_path, 'r') as file:
+                    roles_map = json.load(file)
+            except FileNotFoundError:
+                validations.append(f"ERROR: Couldn't perform kubernetes validations. Reason: File '{roles_map_file_path}' not found.")
+            
+            bindings = roles_response.json()['bindings']
+            for component in apigee_components:
+                if component == "cassandra":
+                    if input_data.get(component, {}).get('backup', '').get('enabled') == True: 
+                        component_sa = input_data.get(component, {}).get('backup', '').get('serviceAccountRef','')
+                    elif input_data.get(component, {}).get('restore', '').get('enabled') == True:
+                        component_sa = input_data.get(component, {}).get('restore', '').get('serviceAccountRef','')
+                    else: 
+                        continue
+                else:
+                    component_sa = input_data.get(component, {}).get('serviceAccountRef', '')
+                
+                success_bool, existing_roles = get_sa_role(component_sa, apigee_namespace, bindings)
+                if success_bool:
+                    req_roles = roles_map.get(f"apigee-{component}", '')
+                    for role in req_roles:
+                        if role not in existing_roles:
+                            validations.append(f"ERROR: Service Account Secret {component_sa} doesn't have required role({role})")
+                else:
+                    error_msg = existing_roles.get("error")
+                    validations.append(f"ERROR: {error_msg}")
+                    
+        else:
+            error_msg = roles_response.get("error")
+            validations.append(f"ERROR: Couldn't retrieve the roles associated with service accounts. Reason : {error_msg}")
+       
     if len(validations) > 0:
         print('Kubernetes validation Errors found !')
         print("\n".join(validations))
